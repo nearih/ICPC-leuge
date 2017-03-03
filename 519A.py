9c4c39e39d39c68419df466b46698ec34a4609f4
nearih <siratan2@illinois.edu>
2017-03-02 18:38:04 -0600

    Create 519A.py

---
+++ 519A.py
@@ -0,0 +1,19 @@
+data = ''
+for i in range(8):
+    s = raw_input()
+    data = data+s
+
+dictb = {'q':9,'r':5,'b':3,'n':3,'p':1,'Q':-9,'R':-5,'B':-3,'N':-3,'P':-1}
+lb = ['q','r','b','n','p','Q','R','B','N','P']
+p1 = 0
+for i in range(len(data)):
+    for j in range(len(lb)):
+        if (data[i] == lb[j]):
+            p1 = p1 + dictb[lb[j]]
+            
+if (p1 > 0):
+    print ("Black")
+elif (p1 == 0):
+    print ("Draw")
+else:
+    print ("White")
